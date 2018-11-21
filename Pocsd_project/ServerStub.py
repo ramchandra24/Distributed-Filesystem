53b97193a70acb574cf4dc3161cc3719756948f6
Ram <r.kulkarni@ufl.edu>
2018-11-21 10:30:45 -0500

    Spawning 8 localhost servers

--- Pocsd_project/ServerStub.py
+++ Pocsd_project/ServerStub.py
@@ -1,13 +1,28 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+import threading
 import Memory_RPC as Memory
+import config
 
-server = SimpleXMLRPCServer(("localhost", 8000), allow_none=True)
-print "Listening on localhost : port 8000"
-#Register all functions required for RPC
-server.register_instance(Memory.Operations())
-server.register_introspection_functions()
+class RaidServer():
+    def __init__(self, server_port):
+        server = SimpleXMLRPCServer(("localhost", server_port), allow_none=True)
+        print "Listening on localhost : port ", server_port
+        #Register all functions required for RPC
+        server.register_instance(Memory.Operations())
+        server.register_introspection_functions()
+        #Start the server
+        server.serve_forever()
 
-#Start the server
-server.serve_forever()
 
+server_port = config.SERVER_PORT_BEGIN
+for server_num in range(config.NUM_OF_SERVERS):
+    print "spawning server ", server_num
+    server_handle = threading.Thread(target=RaidServer, args = (server_port, ))
+    server_port = server_port + 1
+    #server_handle.daemon = True
+    server_handle.start()
 
+for server_num in range(config.NUM_OF_SERVERS):
+    server_handle.join();
+    
+print "All threads closed"
