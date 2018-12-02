577682c97000252a132e46835978e99f17089f2b
Ram <r.kulkarni@ufl.edu>
2018-12-02 11:37:37 -0500

    XOR modularized

--- Pocsd_project/VirtualBlockLayer.py
+++ Pocsd_project/VirtualBlockLayer.py
@@ -32,7 +32,7 @@ class Operations():
                 return inode
         return False
 
-
+    # Find XOR of 2 data blocks
     def xor_block_data(self, block1_data, block2_data):
         int_block1_data = []
         for i in range(len(block1_data)):
@@ -55,10 +55,9 @@ class Operations():
         for i in range(config.BLOCK_SIZE):
             if int_xored_block[i] != 0:
                 xored_block[i] = chr(int_xored_block[i])
-        #print "parity data str ", new_updated_parity
         return ''.join(xored_block)
 
-
+    # Collect data from active servers and provide data of the faulty server
     def correct_data_from_parity_server(self, faulty_server, block_number):
         parity_server_num = config.NUM_OF_SERVERS - 1
         if block_number in self.parity_mapper:
@@ -80,20 +79,12 @@ class Operations():
 
     #REQUEST THE DATA FROM THE SERVER
     def get_data_block(self, block_number):
-        #self.print_vnodes()
         server1, block_num1 = self.get_server1_and_block_num(block_number)
-        #server2, block_num2 = self.get_server2_and_block_num(block_number)
-        #parity_server, parity_blknum = self.get_parity_server_and_block_num(block_number)
-        #print "Read from Data Servers : ", server1, ", ",  server2
         stime = config.SLEEP_TIME
-        print "waiting ", stime, " seconds before first read"
-        time.sleep(stime)
         ret_data = ''
         
-        print "Reading from Server: ", server1
         data1 = filesystem.get_data_block(server1, block_num1)
         
-        print data1
         # if nothing was returned from server 1 try server 2
         if data1 == '':
             print "Server ", server1, " not responding"
@@ -101,22 +92,6 @@ class Operations():
             data1 = self.correct_data_from_parity_server(server1, block_num1)
         
         ret_data = data1
-        
-        #print "waiting ", stime, " seconds before second read"
-        #time.sleep(stime)
-        
-        #print "Reading from Server: ", server2
-        #ret_data2 = ''
-        #data2 = filesystem.get_data_block(server2, block_num2)
-        #ret_data2 = data2
-        
-        #if data2 == '':
-        #    print "Server ", server2, " not responding"
-        
-        #if ret_data1 == '':
-        #    ret_data = ret_data2
-        #else:
-        #    ret_data = ret_data1
 
         return ''.join(ret_data)
 
@@ -128,35 +103,15 @@ class Operations():
         server_offset = self.client_blknum_counter % self.num_data_servers
         
         serverA_number = server_offset
-        #serverB_number = (serverA_number + 1) % self.num_data_servers
-        #parity_server_num = config.NUM_OF_SERVERS - 1
-        
         serverA_blknum = filesystem.get_valid_data_block(serverA_number)
-        #serverB_blknum = filesystem.get_valid_data_block(serverB_number)
-        #parity_blknum = filesystem.get_valid_data_block(parity_server_num)
-        
+
         if -1 == serverA_blknum:
-            #print ("Error VirtualBlockLayer: Server " + str(serverA_number) + " not responding")
             serverA_number = (serverB_number + 1) % self.numservers
             serverA_blknum = filesystem.get_valid_data_block(serverA_number)
-            #serverA_number = None
-        #---------------------------------------------- if -1 == serverB_blknum:
-            # #print ("Error VirtualBlockLayer: Server " + str(serverB_number) + " not responding")
-            #----------- serverB_number = (serverB_number + 1) % self.numservers
-            #-- serverB_blknum = filesystem.get_valid_data_block(serverB_number)
-       
-        #----------------------------------------------- if -1 == parity_blknum:
-            # print ("Error VirtualBlockLayer: Server " + str(parity_server_num) + " not responding")
 
         # Push a list of server numbers and block number mappings for each vblock
         server_list[config.DATA_BLOCK_1] = (serverA_number, serverA_blknum)
-        #server_list[config.DATA_BLOCK_2] = (serverB_number, serverB_blknum)
-        
-        
-        #server_list[config.PARITY_BLOCK] = (parity_server_num, parity_blknum)
-        #------------------------------- if serverA_blknum not in parity_mapper:
-            #--------------- self.parity_mapper[serverA_blknum] = serverA_blknum
-        #--------------------------------------------------- self.print_pnodes()
+
         # Create a mapping of Virtual Block Number with list of Actual Block Numbers and their servers
         # Add the new Virtual block -> Actual block mapping to the dictionary
         self.vblock_mapper[self.client_blknum_counter] = server_list
@@ -164,11 +119,13 @@ class Operations():
         # Return the Virtual Block Number to the caller
         return self.client_blknum_counter
     
+    
     def print_vnodes(self):
         print "Vnode size : ", len(self.vblock_mapper), " map:"
         for key, val in self.vblock_mapper.items():
             print key, " : ", val
         return
+    
     
     def print_pnodes(self):
         print "Pnode size : ", len(self.parity_mapper), " map:"
@@ -176,14 +133,16 @@ class Operations():
             print key, " : ", val
         return
 
+
     def get_server1_and_block_num(self, block_number):
-        self.print_vnodes()
+        #self.print_vnodes()
         #server, block_num = self.vblock_mapper[block_number][config.DATA_BLOCK_1]
         if block_number not in self.vblock_mapper:
             return (None, None)
         server_list = self.vblock_mapper[block_number]
         server, block_num = server_list[config.DATA_BLOCK_1]
         return (server, block_num)
+    
     
     def get_server2_and_block_num(self, block_number):
         if block_number not in self.vblock_mapper:
@@ -193,6 +152,7 @@ class Operations():
         server, block_num = server_list[config.DATA_BLOCK_2]
         return (server, block_num)
     
+    
     def get_parity_server_and_block_num(self, block_number):
         if block_number not in self.vblock_mapper:
             return (None, None)
@@ -201,25 +161,17 @@ class Operations():
         server, block_num = server_list[config.PARITY_BLOCK]
         return (server, block_num)
 
+
     #REQUEST TO MAKE BLOCKS RESUABLE AGAIN FROM SERVER
     def free_data_block(self, vblock_number):
         serverA_number, serverA_blknum = self.get_server1_and_block_num(vblock_number)
-        #serverB_number, serverB_blknum = self.get_server2_and_block_num(vblock_number)
-        #parity_server_num, parity_blknum = self.get_parity_server_and_block_num(vblock_number)
-        
+
         status = 0
         #print "Delete block from Data Servers: ", serverA_number, ", ", serverB_number
         if None != serverA_number:
             status = filesystem.free_data_block(serverA_number, serverA_blknum)
         if -1 == status:
             print ("Error VirtualBlockLayer: Server " + str(serverA_number) + " not responding")
-        #-------------------------------------------- if None != serverB_number:
-            # status = filesystem.free_data_block(serverB_number, serverB_blknum)
-        #------------------------------------------------------ if -1 == status:
-            # print ("Error VirtualBlockLayer: Server " + str(serverB_number) + " not responding")
-        #- status = filesystem.free_data_block(parity_server_num, parity_blknum)
-        #------------------------------------------------------ if -1 == status:
-            # print ("Error VirtualBlockLayer: Server " + str(parity_server_num) + " not responding")
 
         # Delete the old Virtual block entry in the dictionary 
         if vblock_number in self.vblock_mapper:
@@ -228,132 +180,35 @@ class Operations():
 
     
     #UPDATED PARITY DATA
-    def get_parity_data(self, vblock_number, block_data):
-        parity_server_num, parity_blknum = self.get_parity_server_and_block_num(vblock_number)
-        parity_block_data = filesystem.get_data_block(parity_server_num, parity_blknum)
-        
-        if parity_block_data[0] == '\0':
-            curr_parity_data = [0] * config.BLOCK_SIZE
-        else:
-            curr_parity_data = map(int, block_data)
-        
-        updated_parity_data = curr_parity_data
-        
-        int_block_data = []
-        for i in range(len(block_data)):
-           int_block_data.append(ord(block_data[i]))
-        for i in range(len(block_data), config.BLOCK_SIZE):
-            int_block_data.append(0)
-
-        for i in range(len(curr_parity_data)):
-            updated_parity_data[i] = curr_parity_data[i] ^ int_block_data[i]
-        
-
-        new_updated_parity = updated_parity_data
-        
-        for i in range(len(updated_parity_data)):
-            if updated_parity_data[i] != 0:
-                new_updated_parity[i] = chr(updated_parity_data[i])
-            else:
-                new_updated_parity[i] = '\0'
-
-        return ''.join(new_updated_parity)
-    
-    #UPDATED PARITY DATA
     def update_parity_data(self, block_number, block_data):
         print "Updating parity for block ", block_number
         parity_server_num = config.NUM_OF_SERVERS - 1
+        # Updating parity
         if block_number in self.parity_mapper:
             parity_blknum = self.parity_mapper[block_number]
+        # Adding parity for the first time for given blocks
         else:
             parity_blknum = filesystem.get_valid_data_block(parity_server_num)
             self.parity_mapper[block_number] = parity_blknum
         
         parity_block_data = filesystem.get_data_block(parity_server_num, block_number)
-        
-        #if parity_block_data[0] == '\0':
-        #    curr_parity_data = [0] * config.BLOCK_SIZE
-        #else:
-        #    curr_parity_data = parity_block_data
-        
-        #print "block data", block_data
-        #updated_parity_data = curr_parity_data
-        #print "parity data int ", updated_parity_data
-        
-        int_block_data = []
-        for i in range(len(block_data)):
-           int_block_data.append(ord(block_data[i]))
-        for i in range(len(block_data), config.BLOCK_SIZE):
-            int_block_data.append(0)
-        
-        int_parity_block_data = []
-        for i in range(len(parity_block_data)):
-           int_parity_block_data.append(ord(parity_block_data[i]))
-        for i in range(len(parity_block_data), config.BLOCK_SIZE):
-            int_parity_block_data.append(0)
 
-        updated_parity_data = [0] * config.BLOCK_SIZE
-        for i in range(config.BLOCK_SIZE):
-            updated_parity_data[i] = int_parity_block_data[i] ^ int_block_data[i]
-        
-        new_updated_parity = updated_parity_data
-        
-        for i in range(config.BLOCK_SIZE):
-            if updated_parity_data[i] != 0:
-                new_updated_parity[i] = chr(updated_parity_data[i])
-            else:
-                new_updated_parity[i] = '\0'
-                
+        new_updated_parity = self.xor_block_data(block_data, parity_block_data)
         filesystem.update_data_block(parity_server_num, parity_blknum, ''.join(new_updated_parity))
-        #print "parity data str ", new_updated_parity
+
         return ''.join(new_updated_parity)
-    
-    
-    def update_parity(self, block_num, block_data):
-        
-        parity_server_num = config.NUM_OF_SERVERS - 1
-        curr_parity = ''
-        if block_num in self.parity_mapper:
-            parity_block_num = self.parity_mapper[block_num]
-            curr_parity = filesystem.get_data_block(parity_server_num, parity_block_num)
-        return
-    
     
     
     #REQUEST TO WRITE DATA ON THE THE SERVER
     def update_data_block(self, vblock_number, block_data):
         server1, block_num1 = self.get_server1_and_block_num(vblock_number)
-        #server2, block_num2 = self.get_server2_and_block_num(vblock_number)
-        #parity_server, parity_blknum = self.get_parity_server_and_block_num(vblock_number)
         
-        #print "Write to Data Servers: ", server1, ", ", server2
-        
-        stime = config.SLEEP_TIME
-        print "waiting ", stime, " seconds before first write"
-        time.sleep(stime)
-        
-        print "Writing to server ", server1
-
         status = filesystem.update_data_block(server1, block_num1, block_data)
-        self.update_parity(block_num1, block_data)
         if -1 == status:
             print ("Error VirtualBlockLayer: Server " + str(server1) + " not responding")
 
         parity_data = self.update_parity_data(block_num1, block_data)
         
-        #--------------- print "waiting ", stime, " seconds before second write"
-        #----------------------------------------------------- time.sleep(stime)
-        #----------------------------------- print "Writing to server ", server2
-        # status = filesystem.update_data_block(server2, block_num2, block_data)
-        #------------------------------------------------------ if -1 == status:
-            # print ("Error VirtualBlockLayer: Server " + str(server2) + " not responding")
-#------------------------------------------------------------------------------ 
-        #--------- parity_data = self.get_parity_data(vblock_number, block_data)
-        # status = filesystem.update_data_block(parity_server, parity_blknum, parity_data)
-        #-------------------------------- print filesystem.status(parity_server)
-        #------------------------------------------------------ if -1 == status:
-            # print ("Error VirtualBlockLayer: Server " + str(server2) + " not responding")
-#------------------------------------------------------------------------------ 
         return
     
     
